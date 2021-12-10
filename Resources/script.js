bbf46b20831c95c61db92ef5515925b6f4af47a4
shwin320 <34398309+shwin320@users.noreply.github.com>
2021-12-10 13:59:11 +0000

    8:59am

--- Resources/script.js
+++ Resources/script.js
@@ -1,1 +1,16 @@
+//Select header image and logo
+var topImg = $("#top");
+var logo = topImg.find("img");
 
+//Add event listener to head image and logo, that checks for scroll
+topImg.scroll(scrollChange);
+logo.scroll(scrollChange);
+
+//When scroll position changes, fades out logo and darkens image
+function scrollChange(){
+    var scrollPosition = document.body.scrollTop; //Get current scroll position
+    if(scrollPosition <= 25){
+        var logoSP = scrollPosition*.01; //Multiplies scroll position by .01, for reducing logo's opacity as user scrolls on page
+        logo.css("opacity", 1-logoSP);
+    }
+}
