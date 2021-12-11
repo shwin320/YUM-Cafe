0e94cc1d168c4d7bb666a0c794bfd489515a273a
shwin320 <34398309+shwin320@users.noreply.github.com>
2021-12-11 01:50:36 +0000

    8:50pm

--- Resources/script.js
+++ Resources/script.js
@@ -7,7 +7,7 @@ function scrollChange(){
     var scrollPosition = window.scrollY; //Get current scroll position
     if(scrollPosition <= 51){
         var logoSP = scrollPosition*.02; //Multiplies scroll position by .02, for reducing logo's opacity as user scrolls on page
-        var darkSP = scrollPosition; //Multiplies scroll position by .01, for darkening header image as user scrolls on page
+        var darkSP = 100-scrollPosition; //Subtracts 100 by scroll position, for darkening header image as user scrolls on page
         
         //Adjusts logo and head image's CSS properties as user scrolls
         topImg.css("filter", "brightness(" + darkSP + "%)");
