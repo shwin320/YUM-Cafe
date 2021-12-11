5554ea3559d83142e7ea405d2e798a0361928d29
shwin320 <34398309+shwin320@users.noreply.github.com>
2021-12-11 01:25:22 +0000

    8:25pm

--- Resources/script.js
+++ Resources/script.js
@@ -4,13 +4,10 @@ var logo = topImg.find("img");
 
 //When scroll position changes, fades out logo and darkens image
 function scrollChange(){
-    var scrollPosition = document.getElementById("top").scrollTop; //Get current scroll position
+    var scrollPosition = document.body.scrollTop; //Get current scroll position
     console.log(scrollPosition);
     if(scrollPosition <= 25){
         var logoSP = scrollPosition*.04; //Multiplies scroll position by .04, for reducing logo's opacity as user scrolls on page
         logo.css("opacity", 1-logoSP);
     }
 }
-
-//Add event listener to body, that checks for scroll
-document.body.addEventListener(onscroll, scrollChange);
