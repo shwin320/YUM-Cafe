472d48ba868c6ff988c930a828a613dd8e2dc6d9
shwin320 <34398309+shwin320@users.noreply.github.com>
2022-03-17 21:50:22 -0400

    Update script.js

--- Resources/script.js
+++ Resources/script.js
@@ -5,12 +5,11 @@ var logo = topImg.find("img");
 //When scroll position changes, fades out logo and darkens image
 function scrollChange(){
     var scrollPosition = window.scrollY; //Get current scroll position
-    if(scrollPosition <= 51){
-        var logoSP = scrollPosition*.02; //Multiplies scroll position by .02, for reducing logo's opacity as user scrolls on page
-        var darkSP = 100-scrollPosition; //Subtracts 100 by scroll position, for darkening header image as user scrolls on page
+    var logoSP = scrollPosition*.02; //Multiplies scroll position by .02, for reducing logo's opacity as user scrolls on page
+    var darkSP = 100-scrollPosition; //Subtracts 100 by scroll position, for darkening header image as user scrolls on page
         
-        //Adjusts logo and head image's CSS properties as user scrolls
-        topImg.css("filter", "brightness(" + darkSP + "%)");
-        logo.css("opacity", 1-logoSP);
+    //Adjusts logo and head image's CSS properties as user scrolls
+    topImg.css("filter", "brightness(" + darkSP + "%)");
+    logo.css("opacity", Math.max((1-logoSP), 0));
     }
 }
